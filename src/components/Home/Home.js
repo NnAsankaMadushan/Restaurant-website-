669ebabb2d0d865578150b7c410b1355e7013ea3
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-07-11 05:08:19 +0530

    added about

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -27,9 +27,19 @@ const Home = () => {
         </div>
       </nav>
       <div className='homeContainer'>
-        <img src={brandIcon} alt="Brand Icon" />
+        <section className='about'>
+          <img src={brandIcon} alt="Brand Icon" />
+          <div className='about-description'>
+            <p>Welcome to <span>Burgers</span>, where flavor meets quality! Since our founding in 2010, we have been committed to crafting the most delicious and satisfying burgers in town. Our journey began with a simple yet powerful idea: to create a place where people could enjoy high-quality, gourmet burgers made from the freshest ingredients.</p>
+
+            <p>Thank you for choosing Burgers. Whether you’re dining in, taking out, or ordering delivery, we are committed to providing you with an unforgettable dining experience. Join us today and discover why we’re not just serving food, but creating memories.</p>
+
+            <p>Burgers - Taste the Difference.</p>
+          </div>
+        </section>
       </div>
     </div>
+    
   );
 }
 
