d7eaa791dc298b706778d548840cc4683c20aef3
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-07-10 15:22:15 +0530

    add navigation bar

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Signup.css';
 import profileImage from '../Images/profile1.png';
+import brandIcon from '../Images/profile1.png'; // Replace with your brand icon
 import { SocialIcon } from 'react-social-icons';
 import axios from 'axios';
 
@@ -29,57 +30,73 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
-      <div className={`form-container ${isFlipped ? 'flipped' : ''}`}>
-        <form className="form signup-form" onSubmit={handleSubmit}>
-          <img src={profileImage} alt="Grand Restaurant Logo" className="logo" />
-          <h1 className="title">
-            <span style={{ color: 'red', fontSize: '40px' }}>Welcome</span>&nbsp;
-            <span style={{ color: 'orange' }}>Back !</span>
-          </h1>
-          <p className="subtitle">Enter your details to access your account</p>
-          <input type="email" className="input" placeholder="Enter your email address" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input type="password" className="input" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <input type="password" className="input" placeholder="Confirm password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-          <div className="checkbox-wrapper">
-            <input type="checkbox" className="checkbox" />
-            <label className="label">Accept terms and conditions</label>
+    <>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <img src={brandIcon} alt="Brand Icon" />
+          <span>Burgers</span>
+        </div>
+        <div className="navbar-info">
+          <div>Email: info@brand.com</div>
+          <div>Phone: (123) 456-7890</div>
+          <div className='signinup'>
+            <a href="#" onClick={() => setIsFlipped(false)}>Sign Up</a>
+            <a href="#" onClick={() => setIsFlipped(true)}>Sign In</a>
           </div>
-          <button className="button">Sign Up</button>
-          <p className="text-signin">Or sign in with</p>
-          <div className="social-buttons">
-            <button className="google-button">
-              <SocialIcon url="https://google.com" className="custom-social-icon" />Sign in with Google
-            </button>
-            <button className="facebook-button">
-              <SocialIcon url="https://facebook.com" className="custom-social-icon" />Sign in with Facebook
-            </button>
-          </div>
-          <button className="flip-button" type="button" onClick={() => setIsFlipped(!isFlipped)}>Sign In</button>
-        </form>
-        <form className="form signin-form" onSubmit={handleSigninSubmit}>
-          <img src={profileImage} alt="Grand Restaurant Logo" className="logo" />
-          <h1 className="title">
-            <span style={{ color: 'red', fontSize: '40px' }}>Welcome</span>&nbsp;
-            <span style={{ color: 'orange' }}>Back !</span>
-          </h1>
-          <p className="subtitle">Enter your details to sign in to your account</p>
-          <input type="email" className="input" placeholder="Enter your email address" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input type="password" className="input" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <button className="button">Sign In</button>
-          <p className="text-signin">Or sign in with</p>
-          <div className="social-buttons">
-            <button className="google-button">
-              <SocialIcon url="https://google.com" className="custom-social-icon" />Sign in with Google
-            </button>
-            <button className="facebook-button">
-              <SocialIcon url="https://facebook.com" className="custom-social-icon" />Sign in with Facebook
-            </button>
-          </div>
-          <button className="flip-button" type="button" onClick={() => setIsFlipped(!isFlipped)}>Sign Up</button>
-        </form>
+        </div>
+      </nav>
+      <div className="logincontainer">
+        <div className={`form-container ${isFlipped ? 'flipped' : ''}`}>
+          <form className="form signup-form" onSubmit={handleSubmit}>
+            <img src={profileImage} alt="Grand Restaurant Logo" className="logo" />
+            <h1 className="title">
+              <span style={{ color: 'red', fontSize: '40px' }}>Welcome</span>&nbsp;
+              <span style={{ color: 'orange' }}>Back !</span>
+            </h1>
+            <p className="subtitle">Enter your details to access your account</p>
+            <input type="email" className="input" placeholder="Enter your email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" className="input" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input type="password" className="input" placeholder="Confirm password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+            <div className="checkbox-wrapper">
+              <input type="checkbox" className="checkbox" />
+              <label className="label">Accept terms and conditions</label>
+            </div>
+            <button className="button">Sign Up</button>
+            <p className="text-signin">Or sign in with</p>
+            <div className="social-buttons">
+              <button className="google-button">
+                <SocialIcon url="https://google.com" className="custom-social-icon" />Sign in with Google
+              </button>
+              <button className="facebook-button">
+                <SocialIcon url="https://facebook.com" className="custom-social-icon" />Sign in with Facebook
+              </button>
+            </div>
+            <button className="flip-button" type="button" onClick={() => setIsFlipped(true)}>Sign In</button>
+          </form>
+          <form className="form signin-form" onSubmit={handleSigninSubmit}>
+            <img src={profileImage} alt="Grand Restaurant Logo" className="logo" />
+            <h1 className="title">
+              <span style={{ color: 'red', fontSize: '40px' }}>Welcome</span>&nbsp;
+              <span style={{ color: 'orange' }}>Back !</span>
+            </h1>
+            <p className="subtitle">Enter your details to sign in to your account</p>
+            <input type="email" className="input" placeholder="Enter your email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" className="input" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button className="button">Sign In</button>
+            <p className="text-signin">Or sign in with</p>
+            <div className="social-buttons">
+              <button className="google-button">
+                <SocialIcon url="https://google.com" className="custom-social-icon" />Sign in with Google
+              </button>
+              <button className="facebook-button">
+                <SocialIcon url="https://facebook.com" className="custom-social-icon" />Sign in with Facebook
+              </button>
+            </div>
+            <button className="flip-button" type="button" onClick={() => setIsFlipped(false)}>Sign Up</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
