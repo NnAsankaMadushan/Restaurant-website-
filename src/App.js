428c6ff1695f066911d4923f1408cffb96440037
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-07-10 11:35:08 +0530

    fixing signup box

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Signup from './components/Signup';
+import Signup from './components/Signup/Signup';
 ;
 export default function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
+                <Route path="/" element={<Signup />} />
+                {/* <Route path="/signup" element={<Signup />} /> */}
             </Routes>
         </Router>
     );
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './Login.css';
+import './Signup.css';
 import profileImage from '../Images/profile1.png';
+import ReactDOM from 'react-dom'
+import { SocialIcon } from 'react-social-icons'
+
 const Login = () => {
   return (
     <div className="container">
       <div className="form">
         <img src={profileImage} alt="Grand Restaurant Logo" className="logo" />
-        <h1 className="title">Welcome Back</h1>
+        <h1 className="title">
+          <span style={{ color: 'red', fontSize: '40px'  }}>Welcome</span>
+          &nbsp;
+          <span style={{ color: 'orange'}}>Back !</span>
+        </h1>
+
         <p className="subtitle">Enter your details to access to your account</p>
         <input type="email" className="input" placeholder="Enter your email address" />
         <input type="password" className="input" placeholder="Password" />
@@ -18,8 +26,12 @@ const Login = () => {
         <button className="button">Sign Up</button>
         <p className="text-signin">Or sign in with</p>
         <div className="social-buttons">
-          <button className="google-button">Sign in with Google</button>
-          <button className="facebook-button">Sign in with Facebook</button>
+          <button className="google-button">
+          <SocialIcon url="www.google.com" className="custom-social-icon" />Sign in with Google
+          </button>
+          <button className="facebook-button">
+            <SocialIcon url="www.facebook.com" className="custom-social-icon" />Sign in with Facebook
+          </button>
         </div>
       </div>
     </div>
