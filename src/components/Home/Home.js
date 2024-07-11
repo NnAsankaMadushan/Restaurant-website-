15728c4bb3b7752e2a0f9710d0206971bd476795
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-07-11 13:28:12 +0530

    contact page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import brandIcon from '../Images/profile1.png';
 import food1 from '../Images/foods/food-1.png';
 import food2 from '../Images/foods/food-2.png';
@@ -7,13 +7,28 @@ import food4 from '../Images/foods/food-4.png';
 import food5 from '../Images/foods/food-5.png';
 import food6 from '../Images/foods/food-6.png';
 import './Home.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Home = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const foodImgsRef = useRef(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  useEffect(() => {
+    const scrollInterval = setInterval(() => {
+      if (foodImgsRef.current) {
+        foodImgsRef.current.scrollBy({
+          left: 200, // Adjust this value for the desired scroll distance
+          behavior: 'smooth',
+        });
+      }
+    }, 2000); // Adjust this value for the desired scroll interval (in milliseconds)
+
+    return () => clearInterval(scrollInterval);
+  }, []);
 
   return (
     <div className='container'>
@@ -47,7 +62,7 @@ const Home = () => {
               <p>Burgers - Taste the Difference.</p>
             </div>
           </div>
-          <div className='food-imgs'>
+          <div className='food-imgs' ref={foodImgsRef}>
             <img src={food1} alt="food" />
             <img src={food2} alt="food" />
             <img src={food3} alt="food" />
@@ -67,13 +82,14 @@ const Home = () => {
         <h1>Contact</h1>
         <section className='contact'>
           <div className='about-description'>
-            <div>
-              <p>Welcome to <span>Burgers</span>, where flavor meets quality! Since our founding in 2010, we have been committed to crafting the most delicious and satisfying burgers in town. Our journey began with a simple yet powerful idea: to create a place where people could enjoy high-quality, gourmet burgers made from the freshest ingredients.</p>
-
-              <p>Thank you for choosing Burgers. Whether you’re dining in, taking out, or ordering delivery, we are committed to providing you with an unforgettable dining experience. Join us today and discover why we’re not just serving food, but creating memories.</p>
-
-              <p>Burgers - Taste the Difference.</p>
+            <div className='contactInfo'>
+              <p><i className="fas fa-phone"></i> Telephone Number : 011 2 345 458</p>
+              <p><i className="fas fa-envelope"></i> Email : burgers@gmail.com</p>
+              <p><i className="fas fa-globe"></i> Website : www.burgers.com</p>
+              <p><i className="fab fa-facebook"></i> Facebook : www.facebook.com/burgers</p>
+              <p><i className="fab fa-facebook"></i> Facebook : www.facebook.com/burgers</p>
             </div>
+            <img src={brandIcon} alt="Brand Icon" />
           </div>
         </section>
       </div>
